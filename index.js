ce89a1010778c142f57d393f31c72261f8415d1d
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-13 21:48:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+function increaseRankBy(n){
+  let rl=document.querySelectorAll('.ranked-list')
+  n=parseInt(n,10)
+  for(let i=0;i<rl.length;i++){
+    rl[i].innerHTML=(i+n).toString()
+  }
+}
+function deepestChild(){
+  let currentLevel=document.getElementById('grand-node')
+  let nextLevel=currentLevel.children[0]
+  while(nextLevel){
+    currentLevel=nextLevel
+    nextLevel=currentLevel.children[0]
+  }
+  return currentLevel
+}
