e9dd0f87c1ee569e5d3a601130dce7cdd65206e6
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-13 21:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function increaseRankBy(n){
 }
 function deepestChild(){
   let currentLevel=document.getElementById('grand-node')
-  let nextLevel=currentlevel.children
+  let nextLevel=currentlevel.children[0]
   while(nextLevel){
     currentLevel=nextLevel
-    nextLevel=currentLevel.children
+    nextLevel=currentLevel.children[0]
   }
   return currentLevel
 }
